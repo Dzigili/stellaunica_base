07967ac88253824d004aeeb05e88940b3d1dd18c
Igor Vujkovic <igor.v@digitalcube.rs>
2018-09-21 16:59:45 +0200

    changes contact form

--- static/script.js
+++ static/script.js
@@ -82,6 +82,132 @@ function goRight(){
 // TODO srediti css da lici
 
 // TODO naci nacina da se zatvori modal
+
+$(function () {
+    $("#contactSubmit").click(function () {
+        submitVisitorsData();
+       // console.log("Desktop submit btn");
+    });
+});
+
+
+function submitVisitorsData() {
+
+
+    var _name = $('#inputName').val();
+    var _email = $('#inputEmail').val();
+    var _msg = $('#inputMessage').val();
+
+    console.log("Submit all data!", _name, _email, _msg);
+
+    if (_email === '') {
+        replaceContentInContainer('#contact-submit-box-replay-msg', 'Molimo Vas da unesite Vaš e - mail.');
+        $('#inputEmail').css('border', '1px solid #E21900');
+        $('#inputEmail').css('background-color', '#DBDBDB');
+        $('#inputEmail').focus();
+        $('#contact-submit-box-replay-msg').css('color', '#E21900');
+    } else if (_msg === '') {
+        replaceContentInContainer('#contact-submit-box-replay-msg', 'Molimo Vas da unesete poruku.');
+        $('#inputMessage').css('border', '1px solid #E21900');
+        $('#inputMessage').css('background-color', '#DBDBDB');
+        $('#inputMessage').focus();
+        $('#contact-submit-box-replay-msg').css('color', '#E21900');
+    } else {
+        $.ajax({
+            url: '/api/submit',
+            type: 'POST',
+            data: jQuery.param({email: _email, name: _name, message: _msg}),
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            success: function (response) {
+                console.log("RES", response);
+                $('#inputName').val('');
+                $('#inputEmail').val('');
+                $('#inputMessage').val('');
+                $('#contact-submit-box-replay-msg').css('color', '#008000');
+                $('#contactSubmit').css('border', 'unset');
+                replaceContentInContainer('#contact-submit-box-replay-msg', 'Hvala Vam. Vaša poruka je uspešno poslata.');
+
+            },
+            error: function (err) {
+                console.log("ERROR submit all data", err.responseText);
+                if (err.hasOwnProperty('responseText') && JSON.parse(err.responseText).hasOwnProperty('message')) {
+                    if (JSON.parse(err.responseText).message === 'Invalid request argument') {
+                        replaceContentInContainer('#contact-submit-box-replay-msg', 'Vaš e - mail nije tačan.');
+                        $('#contact-submit-box-replay-msg').css('color', '#E21900');
+                        $('#inputEmail').css('border', '1px solid #E21900');
+                        $('#inputEmail').css('background-color', '#DBDBDB');
+                        $('#inputEmail').focus();
+                        return;
+                    } else {
+                        replaceContentInContainer('#contact-submit-box-replay-msg', JSON.parse(err.responseText).message);
+                    }
+                }
+                $('#inputName').css('border', '1px solid #E21900');
+                $('#inputEmail').css('border', '1px solid #E21900');
+                $('#inputMessage').css('border', '1px solid #E21900');
+                $('#contact-submit-box-replay-msg').css('color', '#E21900');
+            }
+        });
+    }
+}
+
+
+function replaceContentInContainer(id, msg){
+    console.log('replaceContentInContainer', id, msg);
+    $(id).html(msg);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#contact-form').submit(function(e) {
+//     var name = $('#inputName'),
+//         email = $('#inputEmail'),
+//         message = $('#inputMessage');
+//
+//     if (!name.value || !email.value || !message.value) {
+//         alertify.error('Proverite uneta polja');
+//     } else {
+//
+//       $.ajax({
+//                 url: '/api/submit',
+//                 type: 'POST',
+//                 data: jQuery.param({email: email, name: name, message: message}),
+//                 contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+//                 success: function (response) {
+//                     console.log("RES", response);
+//                     $(pid + ' #contact-submit-box-name').val('');
+//                     $(pid + ' #contact-submit-box-email').val('');
+//                     $(pid + ' #contact-submit-box-msg').val('');
+//                     replaceContentInContainer(pid + ' #contact-submit-box-replay-msg', 'Thank you. Your message has been sent.');
+//
+//         e.preventDefault();
+//         $(this).get(0).reset();
+//         alertify.success('Poruka je poslata');
+//     }
+//
+// });
+
+
+
+
+
+
+
+
+
+
+
 <!--<script>-->
 
 <!--var stan1 = [-->
